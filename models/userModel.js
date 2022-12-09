f2880046a285840f1b80ff42179e99ee35083aa4
loiderson <100608327+loiderson@users.noreply.github.com>
2022-12-08 16:31:38 -0800

    Create userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,46 @@
+
+const database = [
+  {
+    id: 1,
+    name: "Jimmy Smith",
+    email: "jimmy123@gmail.com",
+    password: "jimmy123!",
+    reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}]
+  },
+  {
+    id: 2,
+    name: "Johnny Doe",
+    email: "johnny123@gmail.com",
+    password: "johnny123!",
+    reminders: [{id: 1, title: "adfasfwer23423", description: "abcabc", completed: false}]
+  },
+  {
+    id: 3,
+    name: "Jonathan Chen",
+    email: "jonathan123@gmail.com",
+    password: "jonathan123!",
+    reminders: []
+  },
+];
+
+const userModel = {
+findOne: (email) => {
+  const user = database.find((user) => user.email === email);
+  if (user) {
+    return user;
+  }
+  throw new Error(`Couldn't find user with email: ${email}`);
+},
+findById: (id) => {
+  const user = database.find((user) => user.id === id);
+  if (user) {
+    return user;
+  }
+  throw new Error(`Couldn't find user with id: ${id}`);
+},
+createNewUser: (userObj) => {
+  database.push(userObj);
+}
+};
+
+module.exports = { database, userModel };
