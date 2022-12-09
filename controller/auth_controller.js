1c0b31a441672ee6e6195b48482c3f63c51e5315
loiderson <100608327+loiderson@users.noreply.github.com>
2022-12-08 16:19:27 -0800

    updated auth_controller according to the assignment

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -1,4 +1,6 @@
-let database = require("../database");
+let database = require("../models/database").userModel;
+const passport = require("../middleware/passport");
+const { userModel } = require("../models/database");
 
 let authController = {
   login: (req, res) => {
@@ -9,12 +11,23 @@ let authController = {
     res.render("auth/register");
   },
 
-  loginSubmit: (req, res) => {
-    // implement
+  loginSubmit: (req, res, next) => {
+    passport.authenticate("local", {successRedirect: "/reminders", failureRedirect: "/login"})(req, res, next);
   },
 
   registerSubmit: (req, res) => {
-    // implement
+    const email = req.body.email
+    const password = req.body.password
+
+    userObj = {
+      id: 1,
+      name: "Deez",
+      email: email,
+      password: password,
+      reminders: []
+    }
+    userModel.createNewUser(userObj) // Call the create new user function to create a new user. returns a user object.
+    res.redirect("/login") // This will redirect to the login page.
   },
 };
 
